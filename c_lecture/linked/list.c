fc795d65349d084bfaa362603174e91e6e46199d
ehgud0670 <kimdo2297@gmail.com>
2019-03-13 14:54:33 +0000

    2019/03/13

--- c_lecture/linked/list.c
+++ c_lecture/linked/list.c
@@ -10,18 +10,21 @@ struct Node{
 struct Node *head= NULL;
 
 void linsert(struct Node *temp, int data){
-    
-  temp = (struct Node*)malloc(sizeof(struct Node));
-  //값 넣기
+  //temp = (struct Node*)malloc(sizeof(struct Node));   malloc을 쓰면 새로운 메모리를 할당하기 때문에 기존의 지역변수가
+  // call by reference가 통하지 않은 것이다.
   temp -> data = data;
   //linked 하기
   temp -> next = head; //처음엔 NULL. head가 처음엔 null이었으니깐.
   head = temp;
   
 }
-
-
-
+void lprint(){
+  struct Node *current = head;
+  while(current){
+    printf("%d ", current -> data);
+    current = current -> next;
+	}
+}
 
 int main(){
   
@@ -30,13 +33,19 @@ int main(){
   //정수 10개 전송
 
   for(i=0; i<10; i++){
-    linsert(&node[i],i+1);    
+    linsert(node + i,i+1);    
 	}
 	
+
+	printf("노드 element 0~9까지 data 값: ");
   for(i=0; i<10; i++){
-     printf("%d \n",node[i].data);  // 내가 보기엔 call by reference로 될 것 같은데 왜 안되는 지 물어보기
+     printf("%d ",(node+i)->data);  // call by reference 안되는 이유 찾았다.
 	}
-  	
+  printf("\n\n");	
+
+  printf("연결리스트 헤드부터 data 값: ");
+  lprint();
+  printf("\n\n");	
 
   return 0;
 }
