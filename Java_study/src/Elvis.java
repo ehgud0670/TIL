842517f90de784b7033dd4696f8c8513816792e4
ehgud0670 <kimdo2297@gmail.com>
2019-05-25 15:12:12 +0900

    기존 src파일 삭제 및 Elvis.java 파일 추가

---
+++ Java_study/src/Elvis.java
@@ -0,0 +1,19 @@
+//public static final 필드 방식의 싱글턴
+public class Elvis {
+    public static final Elvis INSTANCE = new Elvis();
+    private static int count;
+
+    // 생성자는 private 으로 감춰둔다.
+    private Elvis() {
+        count++;
+        if(count>1){
+            throw new IllegalStateException("this object should be singleton");
+        }
+    }
+
+
+    // 어떤 용도인지 물어보기
+    public void leaveTheBuilding() {
+
+    }
+}
