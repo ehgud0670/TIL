71da0482b51b3c0da7de0831baa2787a6a4babb8
ehgud0670 <kimdo2297@gmail.com>
2019-06-11 20:30:15 +0900

    TreeMapExample.java 추가

--- By_date/src/kr/ac/ajou/study0611/TreeMapExample1.java
+++ By_date/src/kr/ac/ajou/study0611/TreeMapExample1.java
@@ -6,9 +6,8 @@ import java.util.TreeMap;
 public class TreeMapExample1 {
     public static void main(String[] args) {
 
-        // note: 점수 기준으로 검색해야 되서 key 값이 Integer 인 것이다.
         TreeMap<Integer, String> scores = new TreeMap<>();
-        scores.put(87, "홍길동");  // note: 기본 타입(int)에서 박스타입(Integer)으로 오토박싱된다.
+        scores.put(87, "홍길동");
         scores.put(98, "이동수");
         scores.put(75, "박길순");
         scores.put(95, "신용권");
@@ -16,11 +15,30 @@ public class TreeMapExample1 {
 
         Map.Entry<Integer, String> entry = null;
 
-        entry = scores.firstEntry();
-        System.out.println("가장 낮은 점수: " + entry.getKey() + " - " + entry.getValue());
+        entry = scores.firstEntry(); // 제일 키가 낮은 객체 반환(리턴).
+        System.out.println("가장 낮은 점수: " + entry.getKey() + "-" + entry.getValue());
 
-        entry = scores.lastEntry();
-        System.out.println("가장 높은 점수: " +entry.getKey() + " - " + entry.getValue());
-        System.out.println("good");
+        entry = scores.lastEntry(); // 제일 키가 높은 객체 반환.
+        System.out.println("가장 높은 점수: " + entry.getKey() + "-" + entry.getValue());
+
+        entry = scores.lowerEntry(95);
+        System.out.println("95점 아래 점수: " + entry.getKey() + "-" + entry.getValue());
+
+        entry = scores.higherEntry(75);
+        System.out.println("75점 위 점수: " + entry.getKey() + "-" + entry.getValue());
+
+        entry = scores.floorEntry(80);
+        System.out.println("80점 이거나 그 아래 점수: " + entry.getKey() + "-" + entry.getValue());
+
+        entry = scores.ceilingEntry(97);
+        System.out.println("97점 이거나 그 위 점수: " + entry.getKey() + "-" + entry.getValue());
+
+
+        System.out.println();
+        while (!scores.isEmpty()) {
+            entry = scores.pollFirstEntry();
+            System.out.println(entry.getKey() + "-" + entry.getValue() +
+                    "(남은 객체 수: " + scores.size() + ")");
+        }
     }
 }
