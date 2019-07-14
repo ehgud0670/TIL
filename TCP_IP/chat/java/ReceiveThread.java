c8c212c447b67c7086aa4af1d17b3ea731d40bea
ehgud0670 <kimdo2297@gmail.com>
2019-07-14 20:58:47 +0900

    ReceiveThread.java 파일 수정

--- TCP_IP/chat/java/ReceiveThread.java
+++ TCP_IP/chat/java/ReceiveThread.java
@@ -1,10 +1,8 @@
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 
-public class ReceiveThread extends Thread {
+class ReceiveThread extends Thread {
 
     private Socket socket;
 
@@ -14,17 +12,17 @@ public class ReceiveThread extends Thread {
 
     @Override
     public void run() {
-        try {
-            InputStream is = socket.getInputStream();
-            byte[] buf = new byte[512];
 
+        try (InputStream is = socket.getInputStream()) {
             while (true) {
+                byte[] buf = new byte[512];
                 int len = is.read(buf);
-                if(len == -1){
+                if (len == -1) {
+                    System.out.println("[서버 연결 끊김]");
                     break;
                 }
-                System.out.println(new String(buf, 0, len));
-
+                String message = new String(buf, 0, len);
+                System.out.println(message);
             }
         } catch (IOException e) {
             e.printStackTrace();
