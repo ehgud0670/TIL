f5860d8588fc159b31d9e42e5f0a0539074d771a
ehgud0670 <kimdo2297@gmail.com>
2019-07-14 20:59:59 +0900

    SessionThread.java 파일 추가

---
+++ TCP_IP/chat/java/SessionThread.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.List;
+
+class SessionThread extends Thread {
+
+    private Socket socket;
+    private List<Socket> socketList;
+
+    SessionThread(Socket socket, List<Socket> socketList) {
+        this.socket = socket;
+        this.socketList = socketList;
+        this.socketList.add(socket);
+    }
+
+    private void broadcast(String message) throws IOException {
+        for (Socket socket : socketList) {
+            OutputStream os = socket.getOutputStream();
+            os.write(message.getBytes());
+        }
+    }
+
+    @Override
+    public void run() {
+        try (InputStream is = socket.getInputStream()) {
+            while (true) {
+                byte[] buf = new byte[512];
+                int len = is.read(buf);
+                if (len == -1) {
+                    System.out.println("[클라이언트 연결 끊김]");
+                    break;
+                }
+
+                String message = new String(buf, 0, len);
+                broadcast(message);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
