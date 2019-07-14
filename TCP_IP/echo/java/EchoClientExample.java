3b634277f0f813dc06975fab760d9eef2df97ff9
ehgud0670 <kimdo2297@gmail.com>
2019-07-14 16:17:20 +0900

    EchoClientExample.java 파일 수정

--- TCP_IP/echo/java/EchoClientExample.java
+++ TCP_IP/echo/java/EchoClientExample.java
@@ -7,33 +7,38 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoClientExample {
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket();
-            socket.connect(new InetSocketAddress("localhost", 5001));
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket();
+			socket.connect(new InetSocketAddress("localhost", 5001));
 
-            InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();
+			try(InputStream is = socket.getInputStream();
+					OutputStream os = socket.getOutputStream()){
 
-            Scanner sc = new Scanner(System.in);
+				Scanner sc = new Scanner(System.in);
 
-            while(true){
-                String str = sc.nextLine();
-                os.write(str.getBytes());
+				while(true){
+					String str = sc.nextLine();
+					os.write(str.getBytes());
 
-                byte[] buf = new byte[512];
-                int len = is.read(buf);
-                if(len == -1){
-                    break;
-                }
+					byte[] buf = new byte[512];
+					int len = is.read(buf);
+					if(len == -1){
+						break;
+					}
 
-                System.out.println(new String(buf,0,len));
-            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+					String message = new String(buf,0,len);
+					System.out.println(message);
+				}
 
-    }
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
 
