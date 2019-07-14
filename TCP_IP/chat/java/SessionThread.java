aa24918f4f24471b5ab83cfa98f8fc442f13c085
ehgud0670 <kimdo2297@gmail.com>
2019-07-15 00:15:24 +0900

    SessionThread.java 에서 동기화 처리함

--- TCP_IP/chat/java/SessionThread.java
+++ TCP_IP/chat/java/SessionThread.java
@@ -6,40 +6,45 @@ import java.util.List;
 
 class SessionThread extends Thread {
 
-    private Socket socket;
-    private List<Socket> socketList;
+	private Socket socket;
+	private List<Socket> socketList;
+	private static final Object MUTEX = new Object();
 
-    SessionThread(Socket socket, List<Socket> socketList) {
-        this.socket = socket;
-        this.socketList = socketList;
-        this.socketList.add(socket);
-    }
+	SessionThread(Socket socket, List<Socket> socketList) {
+		this.socket = socket;
+		this.socketList = socketList;
+		synchronized(MUTEX){
+			this.socketList.add(socket);
+		}
+	}
 
-    private void broadcast(String message) throws IOException {
-        for (Socket socket : socketList) {
-            OutputStream os = socket.getOutputStream();
-            os.write(message.getBytes());
-        }
-    }
+	private void broadcast(String message) throws IOException {
+		synchronized(MUTEX){
+			for (Socket socket : socketList) {
+				OutputStream os = socket.getOutputStream();
+				os.write(message.getBytes());
+			}
+		}
+	}
 
-    @Override
-    public void run() {
-        try (InputStream is = socket.getInputStream()) {
-            while (true) {
-                byte[] buf = new byte[512];
-                int len = is.read(buf);
-                if (len == -1) {
-                    System.out.println("[클라이언트 연결 끊김]");
-                    break;
-                }
+	@Override
+		public void run() {
+			try (InputStream is = socket.getInputStream()) {
+				while (true) {
+					byte[] buf = new byte[512];
+					int len = is.read(buf);
+					if (len == -1) {
+						System.out.println("[클라이언트 연결 끊김]");
+						break;
+					}
 
-                String message = new String(buf, 0, len);
-                broadcast(message);
+					String message = new String(buf, 0, len);
+					broadcast(message);
 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 }
 
