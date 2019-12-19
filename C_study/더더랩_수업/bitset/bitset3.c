421a75ec4e49fef4b60c6d51115bcb3fdff0950e
ehgud0670 <kimdo2297@gmail.com>
2019-12-19 22:01:08 +0900

    Modified : change the indent interval

--- C_study/더더랩_수업/bitset/bitset3.c
+++ C_study/더더랩_수업/bitset/bitset3.c
@@ -1,5 +1,4 @@
-# include <stdio.h>
-
+#include <stdio.h>
 /*
 TANKER = 00000001
 ~TANKER = 11111110
@@ -13,10 +12,8 @@ REMOVE_ITEM
 
 HAS_ITEM
 변수.item & TANKER;
-
-
 */
-enum item{
+enum item {
   TANKER,// enum의 초깃값은 0이다 .. (명심!)
   HEALER,
   NUKER,
@@ -30,27 +27,19 @@ enum item{
 
 int len = MAX_ITEM_COUNT/(sizeof(unsigned int)*8);
 
-typedef struct {
-  
+typedef struct { 
   unsigned int offset[MAX_ITEM_COUNT/(sizeof(unsigned int)*8)];
-}bitset;
-
+} bitset;
 
 //typedef unsigned int bitset[32];  // => 배열의 decay의 특성을 띄므로 
 // bitset으로 사용하기에 부적절하다. (의미를 다르게 쓸 수 있으므로)
 // 따라서 decay 특성이 없는 struct를 사용하자.
-
-
-
 struct user{
-  //bitset item;
   bitset items;
 };
 
-
 int main(){
-	
-	struct user user; 
+  struct user user; 
   
   printf("%ld \n", sizeof(user.items)/sizeof(unsigned int)); 
 
@@ -60,25 +49,25 @@ int main(){
 
   //HAS_ITEM
   if(user.items.offset[TANKER/len] & 1 << (TANKER % len))
-  	printf("탱커 있음.\n");
+    printf("탱커 있음.\n");
   else 
-  	printf("탱커 없음. \n");
+    printf("탱커 없음. \n");
 
   //ADD_PARKER
   user.items.offset[PARKER/len] |= 1 << (PARKER % len);  
   //HAS_ITEM
   if(user.items.offset[PARKER/len] & 1 << (PARKER % len))
-  	printf("파커 있음.\n");
+    printf("파커 있음.\n");
   else 
-  	printf("파커 없음. \n");
+    printf("파커 없음. \n");
   
   //REMOVE_TANKER
   user.items.offset[TANKER/len] &= ~(1 << TANKER % len);
   //HAS_ITEM
   if(user.items.offset[TANKER/len] & 1 << (TANKER % len))
-  	printf("탱커 있음.\n");
+    printf("탱커 있음.\n");
   else 
-  	printf("탱커 없음. \n");
+    printf("탱커 없음. \n");
 
 
 
