cd4eced8d7cc0004b719484abb701a233345ecae
ehgud0670 <kimdo2297@gmail.com>
2019-12-22 23:31:07 +0900

    Modified : use 'true' reserved word

--- socket/chat/c/chatClient.c
+++ socket/chat/c/chatClient.c
@@ -14,14 +14,14 @@ void *on_client(void *arg){
   
   char buf[128];
   int ret;
-  while(TRUE){
+  while(true){
     ret = read(sock, buf, sizeof buf);
     if(ret <= 0){
       break;
-		}
+    }
 
-		write(1,buf,ret);
-	}
+    write(1,buf,ret);
+  }
 
 }
 
@@ -29,10 +29,10 @@ void *on_client(void *arg){
 int main(){
 
   int sock = socket(PF_INET, SOCK_STREAM , 0);
-  if( sock == -1){
+  if(sock == -1){
     perror("sock");
     return 1;
-	}
+  }
 
   struct sockaddr_in caddr = {0, };
   caddr.sin_family = AF_INET;
@@ -42,21 +42,21 @@ int main(){
   if(connect(sock, (struct sockaddr *)&caddr , sizeof caddr) == -1){
     perror("connect");
     return 1;
-	}
+  }
 
-	pthread_t thread;
-	intptr_t arg = sock;
-	pthread_create(&thread, NULL, on_client, (void*)arg);
+  pthread_t thread;
+  intptr_t arg = sock;
+  pthread_create(&thread, NULL, on_client, (void*)arg);
   pthread_detach(thread);
 
   char buf[128];
   int ret;
-  while(TRUE){
+  while(true){
     ret = read(0, buf, sizeof buf);
     if(ret <= 0 ){
       break;
-		}
-		write(sock,buf, ret);
-	}
-	return 0;
+    }
+    write(sock,buf, ret);
+  }
+  return 0;
 }
--- socket/chat/c/chatServer.c
+++ socket/chat/c/chatServer.c
@@ -8,7 +8,6 @@
 #include <unistd.h>
 
 # define PCOUNT 1024
-# define TRUE 1 
 int n = 0;
 int clients[PCOUNT];
 
@@ -16,37 +15,36 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *on_client(void *arg){
 
-	int sock = (intptr_t)arg;
-	int pos;
+  int sock = (intptr_t)arg;
+  int pos;
 
-	int j;
-	for( j=0 ; j<PCOUNT ; j++){
-		if(clients[j] == 0){
-			break;
-		}
-	}
-	pos = j;
-	clients[j] = sock;
+  int j;
+  for( j=0 ; j<PCOUNT ; j++){
+    if(clients[j] == 0){
+      break;
+    }
+  }
+  pos = j;
+  clients[j] = sock;
 
-	pthread_mutex_lock(&mutex);
-	n++;
-	pthread_mutex_unlock(&mutex);
+  pthread_mutex_lock(&mutex);
+  n++;
+  pthread_mutex_unlock(&mutex);
 
-	while(TRUE){
-		char buf[128];
-		int ret = read(sock, buf, sizeof buf);
-		if( ret <= 0 ){
-			break;
-		}
+  while(true){
+    char buf[128];
+    int ret = read(sock, buf, sizeof buf);
+    if( ret <= 0 ){
+      break;
+    }
 
-		pthread_mutex_lock(&mutex);
-		int i;
-		for(i=0; i<n; i++){
-			write(clients[i], buf, ret);
-		}
-		pthread_mutex_unlock(&mutex);
-
-	}
+    pthread_mutex_lock(&mutex);
+    int i;
+    for(i=0; i<n; i++){
+      write(clients[i], buf, ret);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
   
   clients[pos] = 0;
   
@@ -63,20 +61,20 @@ int main(){
   if( sd == -1){
     perror("socket");
     return 1;
-	}
+  }
 
-	struct sockaddr_in saddr = {0,};
-	saddr.sin_family = AF_INET;
-	saddr.sin_addr.s_addr = INADDR_ANY;
-	saddr.sin_port = htons(5001); // little Endian => Big Endian
-	bind(sd, (struct sockaddr*)&saddr, sizeof saddr );
+  struct sockaddr_in saddr = {0,};
+  saddr.sin_family = AF_INET;
+  saddr.sin_addr.s_addr = INADDR_ANY;
+  saddr.sin_port = htons(5001); // little Endian => Big Endian
+  bind(sd, (struct sockaddr*)&saddr, sizeof saddr );
 
-	listen(sd, SOMAXCONN);
+  listen(sd, SOMAXCONN);
 
-	struct sockaddr_in caddr = {0,};
-	socklen_t caddrlen = sizeof caddr;
+  struct sockaddr_in caddr = {0,};
+  socklen_t caddrlen = sizeof caddr;
 
-	while(TRUE){
+  while(true){
     int sock = accept(sd, (struct sockaddr *)&caddr, &caddrlen);
     intptr_t arg = sock;
 
@@ -85,7 +83,7 @@ int main(){
     pthread_t thread;
     pthread_create(&thread, NULL, on_client, (void*)arg);
     pthread_detach(thread);
-	}
+  }
   
   return 0;
 }
